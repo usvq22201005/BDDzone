d83013df14ee3f9d9d4c0d64a3af48d167265f63
22104859 <arnaud.bacle@ens.uvsq.fr>
2025-12-08 11:36:24 +0100

    ajout tables (1)

--- 00_creation_tables.sql
+++ 00_creation_tables.sql
@@ -21,31 +21,59 @@ create table produit (
 --3.	Commande (CommandeId, ClientId, 
 --DateCommande, PrixTotal)
 create table Commande(
-    CommandeId number 2,
-    CLientId number 5,
-
+    CommandeId number (5),
+    ClientId number(5),
+    DateCommande DATE
     ) ;
 --4.	ProduitCommande (CommandeId, ProduitId,
 --Quantite, Prix) 
 create table ProduitCommande(
-        CommandeId number 10
-        Quantite,
-        Prix ,
+        CommandeId number(6)
+        Quantite number(5),
+        Prix number(4),
 )   
---5.	SouhaiteAcheter (ClientId, ProduitId, Quantite, Prix) 
-
+--5.	SouhaiteAcheter(ClientId, ProduitId, Quantite, Prix) 
+create table SouhaiteAcheter(
+    Clientid number(5),
+    ProduitId number(6),
+    Quantite number(5),
+    Prix(4)
+)
 --6.	Fournisseur (FournisseurId, Nom, Pays, NoteFournisseur) 
-
+create table (FournisseurId, 
+    Nom varchar(20),
+    Pays varchar(30),
+    NoteFournisseur number(2)
+  )
 --7.	Pays (NomPays, ProduitId) 
-
+create table Pays (
+     NomPays varchar(20),
+     ProduitId number(2)
+     )
 --8.	Categorie (CategorieId, Nom, DateAjout) 
-
+create table Categorie(
+    CategorieId number(5),
+    Nom varchar(20),
+    DateAjout DATE
+)
 --9.	SousCategorie (SousCategorieId, CategorieId, Nom, DateAjout) 
-
+create table SousCategorie(
+    SousCategorieId number(5),
+    CategorieId number(5),
+    Nom varchar(20),
+    DateAjout DATE
+)
 --10.	CategorieSousCategorie (CSCId, CategorieId, SousCategorieId)
-
+create table CategorieSousCategorie(
+    CSCId number(5),
+    CategorieId number(5),
+    SousCategorieId number(5)
+)
 --11.	CentreDInteret (ClientId, CSCId)  
-
+create table CentreDInteret(
+    ClientId,
+    CSCId
+    )
 --12.	Favori (ClientId, CSCId)
 
 --13.	NoteProduit (ClientId, ProduitId, Note) 
