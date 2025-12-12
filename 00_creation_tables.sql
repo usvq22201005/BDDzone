f989e24dec02133a93f7734f5b569d5cea2b8524
22104859 <arnaud.bacle@ens.uvsq.fr>
2025-12-12 21:39:41 +0100

    table (7) correction erreurs et oubli mineurs + standard prix + ajout clées primaires

--- 00_creation_tables.sql
+++ 00_creation_tables.sql
@@ -8,7 +8,10 @@ create table client(
     Prenom varchar(50),
     Adresse varchar(50),
     Pays varchar(15),
-    aLocal number(1)
+    aLocal number(1),
+
+    constraint PK_Client primary key (ClientId)
+    -- on evite les doublons...
 );
 -- 2.	Produit (ProduitId, FournisseurId, CategorieId,
 -- SousCategorieId, Nom, Prix, NoteProduit) 
@@ -16,8 +19,11 @@ create table Produit (
     ProduitId number(6),
     FournisseurId number(5),
     CategorieId number(5),
-    SousCategorieId number(5)
-
+    SousCategorieId number(5),
+    Nom varchar(50),
+    Prix number(10,2),  -- Valeur max=99999999.99
+    NoteProduit number(2),
+    constraint PK_Produit primary key (ProduitId)
 );
 --3.	Commande (CommandeId, ClientId, 
 --DateCommande, PrixTotal)
@@ -25,7 +31,8 @@ create table Commande(
     CommandeId number (5),
     ClientId number(5),
     DateCommande DATE,
-    PrixTotal(4)
+    PrixTotal number(10,2),
+    constraint PK_Commande primary key (CommandeId)
 );
 --4.	ProduitCommande (CommandeId, ProduitId,
 --Quantite, Prix) 
@@ -33,27 +40,31 @@ create table ProduitCommande(
         CommandeId number(5),
         ProduitId number(6),
         Quantite number(5),
-        Prix number(4)
+        Prix number(10,2),
+        constraint PK_ProduitCommande primary key (CommandeId, ProduitId)
 );  
 --5.	SouhaiteAcheter (ClientId, ProduitId, Quantite, Prix) 
 create table SouhaiteAcheter(
     ClientId number(5),
     ProduitId number(6),
     Quantite number(5),
-    Prix(4)
+    Prix number(10,2),
+    constraint PK_SouhaiteAcheter primary key (ClientId, ProduitId)
 );
 --6.	Fournisseur (FournisseurId, Nom, Pays, NoteFournisseur) 
 create table Fournisseur(
     FournisseurId number(5), 
     Nom varchar(20),
     Pays varchar(30),
-    NoteFournisseur number(2)
+    NoteFournisseur number(2),
+    constraint PK_Fournisseur primary key (FournisseurId)
 );
 --7.	Stock (ProduitId, Pays, Disponible)
-create table Stock ( ProduitId, Pays, Disponible
+create table Stock (
     ProduitId number(6),
     Pays varchar(20),
-    Disponible number(1)
+    Disponible number(1),
+    constraint PK_Stock primary key (ProduitId, Pays)
 );
 --8.	Categorie (CategorieId, Nom, DateAjout) 
 create table Categorie(
@@ -66,30 +77,35 @@ create table SousCategorie(
     SousCategorieId number(5),
     CategorieId number(5),
     Nom varchar(20),
-    DateAjout DATE
+    DateAjout DATE,
+    constraint PK_SousCategorie primary key (SousCategorieId)
 );
 --!!!!!! Ajout
 --10.	CategorieSousCategorie (CSCId, CategorieId, SousCategorieId)
 create table CategorieSousCategorie(
     CSCId number(5),
     CategorieId number(5),
-    SousCategorieId number(5)
+    SousCategorieId number(5),
+    constraint PK_CSC primary key (CSCId)
 );
 --11.	CentreDInteret (ClientId, CSCId)  
 create table CentreDInteret(
     ClientId number(5),
-    CSCId number(5)
+    CSCId number(5),
+    constraint PK_CentreInteret primary key (ClientId, CSCId)
 );
 --12.	Favori (ClientId, CSCId)
 create table Favori(
     ClientId number(5), 
-    CSCId number(5)
+    CSCId number(5),
+    constraint PK_Favori primary key (ClientId, CSCId)
 );
 --13.	NoteProduit (ClientId, ProduitId, Note) 
 create table NoteProduit(
     ClientId number(5),
     ProduitId number(6),
-    Note number(2)
+    Note number(2),
+    constraint PK_NoteProduit primary key (ClientId, ProduitId)
 );
 
 
@@ -98,11 +114,13 @@ create table Recommandation (
     RecommandationId number(10),
     ClientId number(5), 
     CSCId number(5),
-    DateHeure DATE
+    DateHeure DATE,
+    constraint PK_Recommandation primary key (RecommandationId)
 );
 
 --15.	RecommandationProduit (RecommandationId, ProduitId)
 create table RecommandationProduit(
     RecommandationId number(10), 
-    ProduitId number(6)
+    ProduitId number(6),
+    constraint PK_RecommandationProduit primary key (RecommandationId, ProduitId)
 );
