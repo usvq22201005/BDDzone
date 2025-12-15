78511346457b9f7c69d9b7af9489e30d47a7db36
Arnaud Bacle <arnaud.bacle@ens.uvsq.fr>
2025-12-15 12:04:54 +0100

    vtest(2)

--- 00_creation_tables_non_fini.sql
+++ 00_creation_tables_non_fini.sql
@@ -1,7 +1,5 @@
 --1.Client (ClientId, NomUtilisateur, Nom, Prenom,
 -- Adresse, Pays, Local)
-
-
 create table client(
     ClientId number(5),
     NomUtilisateur varchar(50),
@@ -10,7 +8,6 @@ create table client(
     Adresse varchar(50),
     Pays varchar(15),
     aLocal number(1),
-
     constraint PK_Client primary key (ClientId) -- on evite les doublons...
 );
 
@@ -28,78 +25,8 @@ create table Categorie(
     CategorieId number(5),
     Nom varchar(20),
     DateAjout DATE,
-    constraint PK_Categorie primary key(CategorieId) );
--- 2.	Produit (ProduitId, FournisseurId, CategorieId,
--- SousCategorieId, Nom, Prix, NoteProduit) 
-create table Produit (
-    ProduitId number(6),
-    FournisseurId number(5),
-    CategorieId number(5),
-    SousCategorieId number(5),
-    Nom varchar(50),
-    Prix number(10,2),  -- Valeur max=99999999.99
-    NoteProduit number(2),
-    constraint PK_Produit primary key (ProduitId),
-
-    constraint FK_Produit_Fournisseur
-        foreign key (FournisseurId)
-        references Fournisseur(FournisseurId),
-    constraint FK_Produit_SousCategorie
-        foreign key (SousCategorieId)
-        references SousCategorie(SousCategorieId)
-
-
+    constraint PK_Categorie primary key(CategorieId)
 );
---3.	Commande (CommandeId, ClientId, 
---DateCommande, PrixTotal)
-create table Commande(
-    CommandeId number (5),
-    ClientId number(5),
-    DateCommande DATE,
-    PrixTotal number(10,2),
-    constraint PK_Commande primary key (CommandeId),
-    
-    --clée etrangères :
-    constraint FK_Commande_Client
-        foreign key (ClientId)
-        references Client(ClientId)
-);
---4.	ProduitCommande (CommandeId, ProduitId,
---Quantite, Prix) 
-create table ProduitCommande(
-        CommandeId number(5),
-        ProduitId number(6),
-        Quantite number(5),
-        Prix number(10,2),
-        constraint PK_ProduitCommande primary key (CommandeId, ProduitId),
-        
-        --clée etrengères :
-        constraint FK_PC_Commande
-        foreign key (CommandeId)
-        references Commande(CommandeId),
-    constraint FK_PC_Produit
-        foreign key (ProduitId)
-        references Produit(ProduitId)
-);  
---5.	SouhaiteAcheter (ClientId, ProduitId, Quantite, Prix) 
-create table SouhaiteAcheter(
-    ClientId number(5),
-    ProduitId number(6),
-    Quantite number(5),
-    Prix number(10,2),
-    constraint PK_SouhaiteAcheter primary key (ClientId, ProduitId),
-
-    constraint FK_SA_Client
-        foreign key (ClientId)
-        references Client(ClientId),
-    constraint FK_SA_Produit
-        foreign key (ProduitId)
-        references Produit(ProduitId)
-
-);
-
-
-
 
 --8.	SousCategorie (SousCategorieId, CategorieId, Nom, DateAjout) 
 create table SousCategorie(
@@ -108,7 +35,6 @@ create table SousCategorie(
     Nom varchar(20),
     DateAjout DATE,
     constraint PK_SousCategorie primary key (SousCategorieId),
-    
     constraint FK_SC_Categorie
         foreign key (CategorieId)
         references Categorie(CategorieId)    
@@ -119,29 +45,121 @@ create table CategorieSousCategorie(
     CSCId number(5),
     CategorieId number(5),
     SousCategorieId number(5),
-    constraint PK_CSC primary key (CSCId)
+    constraint PK_CSC primary key (CSCId),
+    constraint FK_CSC_Categorie foreign key (CategorieId)
+        references Categorie(CategorieId),
+    constraint FK_CSC_SousCategorie foreign key (SousCategorieId)
+        references SousCategorie(SousCategorieId)
 );
+
+--2.	Produit (ProduitId, FournisseurId, CategorieId,
+-- SousCategorieId, Nom, Prix, NoteProduit) 
+create table Produit (
+    ProduitId number(6),
+    FournisseurId number(5),
+    CategorieId number(5),
+    SousCategorieId number(5),
+    Nom varchar(50),
+    Prix number(10,2),  -- Valeur max=99999999.99
+    NoteProduit number(2),
+    constraint PK_Produit primary key (ProduitId),
+    constraint FK_Produit_Fournisseur
+        foreign key (FournisseurId)
+        references Fournisseur(FournisseurId),
+    constraint FK_Produit_Categorie
+        foreign key (CategorieId)
+        references Categorie(CategorieId),
+    constraint FK_Produit_SousCategorie
+        foreign key (SousCategorieId)
+        references SousCategorie(SousCategorieId)
+);
+
+--3.	Commande (CommandeId, ClientId, 
+--DateCommande, PrixTotal)
+create table Commande(
+    CommandeId number (5),
+    ClientId number(5),
+    DateCommande DATE,
+    PrixTotal number(10,2),
+    constraint PK_Commande primary key (CommandeId),
+    --clée etrangères :
+    constraint FK_Commande_Client
+        foreign key (ClientId)
+        references Client(ClientId)
+);
+
+--4.	ProduitCommande (CommandeId, ProduitId,
+--Quantite, Prix) 
+create table ProduitCommande(
+        CommandeId number(5),
+        ProduitId number(6),
+        Quantite number(5),
+        Prix number(10,2),
+        constraint PK_ProduitCommande primary key (CommandeId, ProduitId),
+        --clée etrengères :
+        constraint FK_PC_Commande
+        foreign key (CommandeId)
+        references Commande(CommandeId),
+        constraint FK_PC_Produit
+        foreign key (ProduitId)
+        references Produit(ProduitId)
+);  
+
+--5.	SouhaiteAcheter (ClientId, ProduitId, Quantite, Prix) 
+create table SouhaiteAcheter(
+    ClientId number(5),
+    ProduitId number(6),
+    Quantite number(5),
+    Prix number(10,2),
+    constraint PK_SouhaiteAcheter primary key (ClientId, ProduitId),
+    constraint FK_SA_Client
+        foreign key (ClientId)
+        references Client(ClientId),
+    constraint FK_SA_Produit
+        foreign key (ProduitId)
+        references Produit(ProduitId)
+);
+
 --10.	CentreDInteret (ClientId, CategorieId,SousCategorieId)  
 create table CentreDInteret(
     ClientId number(5),
     CategorieId number(5),
     SousCategorieId number(5),
-    constraint PK_CentreInteret primary key (ClientId, CategorieId, SousCategorieId)
+    constraint PK_CentreInteret primary key (ClientId, CategorieId, SousCategorieId),
+    constraint FK_CI_Client
+        foreign key (ClientId)
+        references Client(ClientId),
+    constraint FK_CI_Categorie
+        foreign key (CategorieId)
+        references Categorie(CategorieId),
+    constraint FK_CI_SousCategorie
+        foreign key (SousCategorieId)
+        references SousCategorie(SousCategorieId)
 );
+
 --11.	Favori (ClientId, CSCId)
 create table Favori(
     ClientId number(5), 
     CategorieId number(5),
     SousCategorieId number(5),
-    constraint PK_Favori primary key (ClientId, CategorieId, SousCategorieId)
+    constraint PK_Favori primary key (ClientId, CategorieId, SousCategorieId),
+    constraint FK_F_Client
+        foreign key (ClientId)
+        references Client(ClientId),
+    constraint FK_F_Categorie
+        foreign key (CategorieId)
+        references Categorie(CategorieId),
+    constraint FK_F_SousCategorie
+        foreign key (SousCategorieId)
+        references SousCategorie(SousCategorieId)
 );
+
 --12.	NoteProduit (ClientId, ProduitId, Note) 
 create table NoteProduit(
     ClientId number(5),
     ProduitId number(6),
     Note number(2),
     constraint PK_NoteProduit primary key (ClientId, ProduitId),
-    
     constraint FK_Note_Client
         foreign key (ClientId)
         references Client(ClientId),
@@ -150,7 +168,6 @@ create table NoteProduit(
         references Produit(ProduitId)
 );
 
-
 --13.	Recommandation (RecommandationId, ClientId, CSCId, DateHeure)
 create table Recommandation (
     RecommandationId number(10),
@@ -158,23 +175,20 @@ create table Recommandation (
     CSCId number(5),
     DateHeure DATE,
     constraint PK_Recommandation primary key (RecommandationId),
-
-    constraint FK_Recommendation foreign key(ClientId),
-    references Client(ClientId)
-    
-    constraint FK_reco_CSC
-    foreign key(CSCId)
-    references CategorieSousCategorie(CSCId)
-
+    constraint FK_Reco_Client foreign key(ClientId)
+        references Client(ClientId),
+    constraint FK_Reco_CSC foreign key(CSCId)
+        references CategorieSousCategorie(CSCId)
 );
 
 --14.	RecommandationProduit (RecommandationId, ProduitId)
 create table RecommandationProduit(
     RecommandationId number(10), 
     ProduitId number(6),
-    constraint PK_RecommandationProduit primary key 
-    (RecommandationId, ProduitId),
-
+    constraint PK_RecommandationProduit primary key (RecommandationId, ProduitId),
+    constraint FK_RP_Recommandation
+        foreign key (RecommandationId)
+        references Recommandation(RecommandationId),
     constraint FK_RP_Produit
         foreign key (ProduitId)
         references Produit(ProduitId)
