fdf93cadaa8153db73dc4e542574e4e848eb2518
Arnaud Bacle <arnaud.bacle@ens.uvsq.fr>
2025-12-15 12:01:52 +0100

    vtest(1)

---
+++ 00_creation_tables_non_fini.sql
@@ -0,0 +1,181 @@
+--1.Client (ClientId, NomUtilisateur, Nom, Prenom,
+-- Adresse, Pays, Local)
+
+
+create table client(
+    ClientId number(5),
+    NomUtilisateur varchar(50),
+    Nom varchar(50),
+    Prenom varchar(50),
+    Adresse varchar(50),
+    Pays varchar(15),
+    aLocal number(1),
+
+    constraint PK_Client primary key (ClientId) -- on evite les doublons...
+);
+
+--6.	Fournisseur (FournisseurId, Nom, Pays, NoteFournisseur) 
+create table Fournisseur(
+    FournisseurId number(5), 
+    Nom varchar(20),
+    Pays varchar(30),
+    NoteFournisseur number(2),
+    constraint PK_Fournisseur primary key (FournisseurId)
+);
+
+--7.	Categorie (CategorieId, Nom, DateAjout) 
+create table Categorie(
+    CategorieId number(5),
+    Nom varchar(20),
+    DateAjout DATE,
+    constraint PK_Categorie primary key(CategorieId) );
+-- 2.	Produit (ProduitId, FournisseurId, CategorieId,
+-- SousCategorieId, Nom, Prix, NoteProduit) 
+create table Produit (
+    ProduitId number(6),
+    FournisseurId number(5),
+    CategorieId number(5),
+    SousCategorieId number(5),
+    Nom varchar(50),
+    Prix number(10,2),  -- Valeur max=99999999.99
+    NoteProduit number(2),
+    constraint PK_Produit primary key (ProduitId),
+
+    constraint FK_Produit_Fournisseur
+        foreign key (FournisseurId)
+        references Fournisseur(FournisseurId),
+    constraint FK_Produit_SousCategorie
+        foreign key (SousCategorieId)
+        references SousCategorie(SousCategorieId)
+
+
+);
+--3.	Commande (CommandeId, ClientId, 
+--DateCommande, PrixTotal)
+create table Commande(
+    CommandeId number (5),
+    ClientId number(5),
+    DateCommande DATE,
+    PrixTotal number(10,2),
+    constraint PK_Commande primary key (CommandeId),
+    
+    --clée etrangères :
+    constraint FK_Commande_Client
+        foreign key (ClientId)
+        references Client(ClientId)
+);
+--4.	ProduitCommande (CommandeId, ProduitId,
+--Quantite, Prix) 
+create table ProduitCommande(
+        CommandeId number(5),
+        ProduitId number(6),
+        Quantite number(5),
+        Prix number(10,2),
+        constraint PK_ProduitCommande primary key (CommandeId, ProduitId),
+        
+        --clée etrengères :
+        constraint FK_PC_Commande
+        foreign key (CommandeId)
+        references Commande(CommandeId),
+    constraint FK_PC_Produit
+        foreign key (ProduitId)
+        references Produit(ProduitId)
+);  
+--5.	SouhaiteAcheter (ClientId, ProduitId, Quantite, Prix) 
+create table SouhaiteAcheter(
+    ClientId number(5),
+    ProduitId number(6),
+    Quantite number(5),
+    Prix number(10,2),
+    constraint PK_SouhaiteAcheter primary key (ClientId, ProduitId),
+
+    constraint FK_SA_Client
+        foreign key (ClientId)
+        references Client(ClientId),
+    constraint FK_SA_Produit
+        foreign key (ProduitId)
+        references Produit(ProduitId)
+
+);
+
+
+
+
+--8.	SousCategorie (SousCategorieId, CategorieId, Nom, DateAjout) 
+create table SousCategorie(
+    SousCategorieId number(5),
+    CategorieId number(5),
+    Nom varchar(20),
+    DateAjout DATE,
+    constraint PK_SousCategorie primary key (SousCategorieId),
+    
+    constraint FK_SC_Categorie
+        foreign key (CategorieId)
+        references Categorie(CategorieId)    
+);
+
+--9.	CategorieSousCategorie (CSCId, CategorieId, SousCategorieId)
+create table CategorieSousCategorie(
+    CSCId number(5),
+    CategorieId number(5),
+    SousCategorieId number(5),
+    constraint PK_CSC primary key (CSCId)
+);
+--10.	CentreDInteret (ClientId, CategorieId,SousCategorieId)  
+create table CentreDInteret(
+    ClientId number(5),
+    CategorieId number(5),
+    SousCategorieId number(5),
+    constraint PK_CentreInteret primary key (ClientId, CategorieId, SousCategorieId)
+);
+--11.	Favori (ClientId, CSCId)
+create table Favori(
+    ClientId number(5), 
+    CategorieId number(5),
+    SousCategorieId number(5),
+    constraint PK_Favori primary key (ClientId, CategorieId, SousCategorieId)
+);
+--12.	NoteProduit (ClientId, ProduitId, Note) 
+create table NoteProduit(
+    ClientId number(5),
+    ProduitId number(6),
+    Note number(2),
+    constraint PK_NoteProduit primary key (ClientId, ProduitId),
+    
+    constraint FK_Note_Client
+        foreign key (ClientId)
+        references Client(ClientId),
+    constraint FK_Note_Produit
+        foreign key (ProduitId)
+        references Produit(ProduitId)
+);
+
+
+--13.	Recommandation (RecommandationId, ClientId, CSCId, DateHeure)
+create table Recommandation (
+    RecommandationId number(10),
+    ClientId number(5), 
+    CSCId number(5),
+    DateHeure DATE,
+    constraint PK_Recommandation primary key (RecommandationId),
+
+    constraint FK_Recommendation foreign key(ClientId),
+    references Client(ClientId)
+    
+    constraint FK_reco_CSC
+    foreign key(CSCId)
+    references CategorieSousCategorie(CSCId)
+
+);
+
+--14.	RecommandationProduit (RecommandationId, ProduitId)
+create table RecommandationProduit(
+    RecommandationId number(10), 
+    ProduitId number(6),
+    constraint PK_RecommandationProduit primary key 
+    (RecommandationId, ProduitId),
+
+    constraint FK_RP_Produit
+        foreign key (ProduitId)
+        references Produit(ProduitId)
+);
--- 03_requêtes.sql
+++ 03_requêtes.sql
@@ -1,1 +1,55 @@
-select * from client ;
+-- Pour me faciliter l'existence
+-- je créer des vues : 
+CREATE VIEW V_AchatProduit AS
+SELECT c.ClientId, p.ProduitId,
+ p.CSCId, cmd.DateCommande, l.Qte, l.Prix
+FROM Commande cmd
+JOIN LigneCommande l ON l.
+JOIN Produit p ON 
+JOIN Client c ON 
+
+CREATE VIEW V_Vente AS
+SELECT
+  pc.CommandeId,
+  c.ClientId,
+  p.ProduitId,
+  p.SousCategorieId,
+  sc.CategorieId,
+  pc.Quantite,
+  pc.Prix,
+  co.DateCommande
+FROM ProduitCommande pc
+JOIN Commande co ON pc.CommandeId = co.CommandeId
+JOIN Produit p ON pc.ProduitId = p.ProduitId
+JOIN SousCategorie sc ON p.SousCategorieId = sc.SousCategorieId;
+
+CREATE VIEW V_Recommandation AS
+SELECT
+  r.ClientId,
+  rp.ProduitId,
+  r.DateHeure
+FROM Recommandation r
+JOIN RecommandationProduit rp ON r.RecommandationId = rp.RecommandationId;
+
+CREATE VIEW V_Note AS
+SELECT
+  n.ClientId,
+  n.ProduitId,
+  n.Note
+FROM NoteProduit n;
+
+
+
+-- Quels sont les **produits que souhaite acheter** 
+--un client **donné** qui appartiennent 
+--à des **catégories données ?** Ou à ses favoris ? Qui sont déjà stockés dans son pays ?
+
+select  
+from SouhaiteAcheter SA
+join Produit P on SA.ProduitId= P.ProduitId
+where SA.clientId = client  ;
+
+--Pour chaque client, quelles (sous-)catégories présentes dans son panier n’ont
+--jamais été recommandées
+
+select S
