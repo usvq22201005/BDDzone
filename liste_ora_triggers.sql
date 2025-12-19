d9e6b4a9be84e10e4bf5e54584bb5a0b47321bf2
Aurore-Girod <aurore.girod@ens.uvsq.fr>
2025-12-19 17:52:24 +0100

    Update liste_ora_triggers.sql

--- liste_ora_triggers.sql
+++ liste_ora_triggers.sql
@@ -2,15 +2,15 @@
 
 
 SELECT
-    ut.table_name as "Nom de table",
-    ut.trigger_name as "Nom de trigger",
-    ut.triggering_event as "Evénement de déclenchement",
-    ut.trigger_type as "Type de trigger",
-    ut.status as "Statut",
-    ut.description as "Description",
-    ut.trigger_body as "Corps du trigger"
+    ut.table_name AS "Nom de table",
+    ut.triggering_event AS "Evénement de déclenchement",
+    ut.trigger_type AS "Type de trigger",
+    ut.status AS "Statut",
+    ut.description AS "Description",
+    ut.trigger_body AS "Corps du trigger"
 FROM user_triggers ut
 ORDER BY
     ut.table_name,
     ut.trigger_name;
+
 
