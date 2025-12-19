9d81f3b9cdc03f768822ed8fbb0581d84febdcc0
Aurore-Girod <aurore.girod@ens.uvsq.fr>
2025-12-19 17:47:29 +0100

    Ajout de liste_ora_triggers.sql

---
+++ liste_ora_triggers.sql
@@ -0,0 +1,15 @@
+-- Liste tous les triggers définis classés par table
+
+
+SELECT
+    ut.table_name as Nom de table,
+    ut.trigger_name as Nom de trigger,
+    ut.triggering_event as Evénement de déclenchement,
+    ut.trigger_type as Type de trigger,
+    ut.status as Statut,
+    ut.description as Description,
+    ut.trigger_body as Corps du trigger
+FROM user_triggers ut
+ORDER BY
+    ut.table_name,
+    ut.trigger_name;
